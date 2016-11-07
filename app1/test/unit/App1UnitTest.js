26bb64742a72e8db57d73947060c91a7a71fc81c
Marcelo Bukowski de Farias <marcelo.farias@sencha.com>
2016-11-07 09:21:33 -0800

    Another commit

---
+++ app1/test/unit/App1UnitTest.js
@@ -0,0 +1,9 @@
+describe("App1UnitTest.js", function() {
+    it("should pass", function() {
+        expect(1).toBe(1);
+    });
+    
+    it('should fail', function () {
+        expect(1).toBe(2);
+    })
+});
