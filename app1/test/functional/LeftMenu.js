30ed0903d15f9f98314dd22b64557ebea4fb5311
Marcelo Bukowski de Farias <marcelo.farias@sencha.com>
2016-11-08 15:03:40 -0800

    Add selenium-standalone farm

--- app1/test/functional/LeftMenu.js
+++ app1/test/functional/LeftMenu.js
@@ -1,14 +1,12 @@
 describe("Left menu", function() {
     
     it('navigates through sessions', function() {
-        
         ST.play([
-            { type: "tap", target: "tab[text=\"Users\"]", x: 92, y: 21 },
-            { type: "tap", target: "tab[text=\"Home\"]", x: 90, y: 48 },
-            { type: "tap", target: "mainlist => [data-recordindex=\"0\"]", x: 76, y: 25 },
-            { type: "tap", target: "button[text=\"Yes\"]", x: 64, y: 21 }
+            { type: "tap", target: "tab[text=\"Users\"]", x: 92, y: 43 },
+            { type: "tap", target: "tab[text=\"Home\"]", x: 86, y: 49 },
+            { type: "tap", target: "mainlist => [data-recordindex=\"0\"]", x: 51, y: 15 },
+            { type: "tap", target: "button[text=\"No\"]", x: 37, y: 16 }
         ]);
-        
     });
     
     it('renders correctly', function () {
